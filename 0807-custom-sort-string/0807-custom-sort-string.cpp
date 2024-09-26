161c24ee132df3c496e3412e148e6f0740f51d92
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2024-09-26 23:07:13 +0530

    Time: 0 ms (100%), Space: 7.8 MB (76.4%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -0,0 +1,12 @@
+string str;
+class Solution {
+public:
+    static bool cmp(char a,char b){
+        return (str.find(a)<str.find(b));
+    }
+    string customSortString(string order, string s) {
+        str = order;
+        sort(s.begin(),s.end(),cmp);
+        return s;
+    }
+};
