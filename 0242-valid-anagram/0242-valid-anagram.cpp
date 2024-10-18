6e1468ca6b039af8a776451360713c7496f8029b
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2024-10-19 02:47:02 +0530

    Time: 0 ms (100%), Space: 9 MB (42.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s == t;
+        int freqTable[256]={0};
+        for(int i=0;i<=s.length()-1;i++){
+            freqTable[s[i]]++;
+        }
+        for(int i=0;i<=t.length()-1;i++){
+            freqTable[t[i]]--;
+        }
+        for(int i=0;i<256;i++){
+            if(freqTable[i]!=0)
+            return false;
+        }
+        return true;
     }
 };
