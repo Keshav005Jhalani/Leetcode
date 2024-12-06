06695c6fc451345aeb032053fdd857c5fa3df053
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2024-12-06 23:11:57 +0530

    Time: 7 ms (88.16%), Space: 181.2 MB (88.19%) - LeetHub

--- 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
+++ 3420-find-occurrences-of-an-element-in-an-array/3420-find-occurrences-of-an-element-in-an-array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
-        vector<int> ans;
         vector<int> indice;
         for(int i=0;i<=nums.size()-1;i++){
             if(nums[i]==x){
                 indice.push_back(i);
             }
         }
+        vector<int> ans(queries.size(),-1);
         for(int i=0;i<=queries.size()-1;i++){
-            if(indice.size()<queries[i]) ans.push_back(-1);
-            else ans.push_back(indice[queries[i]-1]);
+            if(indice.size()>=queries[i]) ans[i]=indice[queries[i]-1];
         }
 
         return ans;
