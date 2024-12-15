e766587391abe738aa1b7b96fc4c71f8e1f4d425
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2024-12-15 13:34:04 +0530

    Time: 5 ms (78.18%), Space: 14 MB (35.66%) - LeetHub

--- 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/1320-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -4,11 +4,11 @@ public:
         vector<int> count(s.size());
         int i=0,j=0;
         while(j<=s.size()-1){
-            s[i]=s[j];
             count[i]=1;
             if(i>0 && s[i]==s[i-1]) count[i]+=count[i-1];
             if(count[i]==k) i-=k;
             i++;j++;
+            s[i]=s[j];
         }
         return s.substr(0,i);
     }
