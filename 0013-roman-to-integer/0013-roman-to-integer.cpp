5b534e3f3b651bbf1d34781ad327c06616364f12
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2024-12-18 23:29:35 +0530

    Time: 4 ms (72.26%), Space: 13.4 MB (33.05%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int indexOf(string s) {
-        string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        for (int i = 0; i < 13; i++)
-            if (s == roman[i]) 
-                return i;
-        return -1;
-    }
-
     int romanToInt(string s) {
-        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        int ans = 0;
-        int n = s.size();
-        
-        for (int i = 0; i < n; i++) {
-            if (i + 1 < n && indexOf(s.substr(i, 2)) != -1) {
-                ans += values[indexOf(s.substr(i, 2))];
-                i++;
+        int res = 0;
+        unordered_map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10}, 
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        for (int i = 0; i < s.size() - 1; i++) {
+            if (roman[s[i]] < roman[s[i + 1]]) {
+                res -= roman[s[i]];
             } else {
-                ans += values[indexOf(string(1, s[i]))];
+                res += roman[s[i]];
             }
         }
-        return ans;
+        return res + roman[s[s.size() - 1]];        
     }
 };
