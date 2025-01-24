fda83f5308a2a0cbb72dd87607a60b9c8e1a4d0f
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2025-01-25 03:35:38 +0530

    Time: 3 ms (69.55%), Space: 15.8 MB (28.94%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    void solve(vector<string> &ans,string &output,int open,int close,int n){
+    void solve(vector<string> &ans,string output,int open,int close,int n){
         if(output.size()==n*2){
             ans.push_back(output);
             return;
         }
         //open bracket if
         if(open==close||open<n){
-            output.push_back('(');
-            solve(ans,output,open+1,close,n);
-            output.pop_back();
+            solve(ans,output+"(",open+1,close,n);
         }
         //close bracket if
         if(close<open){
-            output.push_back(')');
-            solve(ans,output,open,close+1,n);
-            output.pop_back();
+            solve(ans,output+")",open,close+1,n);
         }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
         string output;
-        solve(ans,output,0,0,n);
+        solve(ans,"",0,0,n);
         return ans;
     }
 };
