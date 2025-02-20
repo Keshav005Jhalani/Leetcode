215a2a58f53214292d40d941adb2c5cdf35757c8
Keshav005Jhalani <142431893+Keshav005Jhalani@users.noreply.github.com>
2025-02-20 21:33:54 +0530

    Time: 50 ms (49.24%), Space: 83.5 MB (56.98%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    class Car {
+    public:
+        int pos;
+        int speed;
+        Car(int p, int s) : pos(p), speed(s) {}
+    };
+
+    static bool myComp(Car &a, Car &b) {
+        return a.pos > b.pos;
+    }
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        if (n == 0) return 0;
+        vector<Car> cars;
+        for (int i = 0; i < n; i++) {
+            cars.push_back(Car(position[i], speed[i]));
+        }
+        sort(cars.begin(), cars.end(), myComp);
+        stack<float> st; 
+        for (auto car : cars) {
+            float time = (target - car.pos) / (float)car.speed;
+            if (!st.empty() && time <= st.top()) {
+                continue; 
+            }   
+            st.push(time);
+        }
+        return st.size();
+    }
+};
